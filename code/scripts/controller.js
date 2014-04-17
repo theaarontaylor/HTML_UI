61b7aec1dccca4921eba8a7b36ed95e0bed80ee2
atsvetov <fict.at.ts@gmail.com>
2014-04-17 14:27:45 +0300

    Assignment angular controller

--- code/scripts/controller.js
+++ code/scripts/controller.js
@@ -1,10 +1,13 @@
 
 var assignLst = angular.module('assignLst', []);
+
 assignLst.controller('AssignLstCtrl', ['$scope','$http', function ($scope, $http) {
-  $http.get('http://u.cs.biu.ac.il/~altshus/demo.json').success(function(data) {
-    $scope.events = data.events;
+  $http.get('http://whatsdueapp.com/users/dan/assignments/POL%20234.json').success(function(data) {
+    $scope.events = data;
 	console.log(data);	
+
   });
  
   $scope.orderProp = 'time';
 }]);
+
